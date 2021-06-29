6b11dcb8ecad7e5dd5d9b99694e2f707110a8fb5
Norbert Kunszt <kunszt.nirbert@gmail.com>
2021-06-29 17:28:12 +0200

    home develop +fix error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  imgs=new Array(8)
   cards:Array<object>=[
     {title:'home',link:'/',content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque vestibulum lorem ac facilisis. Etiam ornare, felis sed consectetur efficitur, neque ipsum facilisis nisi, a dapibus justo nunc a massa. Praesent sagittis dui tellus, et iaculis est ornare sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla nec mi elementum, sodales nulla vitae, dictum lorem. Cras vestibulum dui urna, in imperdiet lectus elementum sed. Vestibulum porttitor quam sem, et finibus ligula rhoncus id. Nullam pulvinar justo sed pretium ornare. Nullam condimentum interdum pharetra. Quisque eu accumsan mauris, et scelerisque nisl. Quisque consequat porttitor elit, nec molestie orci aliquam at. Phasellus porta quis tellus eget rutrum. Proin nec tortor eget ipsum cursus rutrum eget sit amet ex.',btnTitle:'Go To'},
     {title:'chat',link:'/chat',content:'Aenean pulvinar, arcu nec fermentum ornare, ante massa aliquam dui, et posuere odio quam rhoncus metus. Mauris rutrum, odio eget vehicula rhoncus, erat dui euismod ligula, sed porttitor tortor ante sit amet ante. Nullam interdum egestas purus at commodo. Aliquam in volutpat nulla. Duis sem diam, aliquet ut tristique quis, faucibus at libero. Donec nec mi eget odio facilisis semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam luctus purus id leo vulputate sagittis. Praesent tincidunt pulvinar malesuada. Nam nulla turpis, sodales at tellus at, pharetra egestas mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id venenatis est. Integer molestie erat ac mollis suscipit. Vestibulum pellentesque orci nulla, a blandit sem lobortis non.',btnTitle:'Megnyit'},
@@ -14,16 +14,16 @@ export class HomeComponent implements OnInit {
     {title:'video call',link:'/video',content:'Maecenas bibendum ac nibh quis porta. Curabitur eget dapibus mi. Aliquam felis lorem, consequat sed porttitor eu, fringilla id turpis. Suspendisse potenti. Sed condimentum blandit quam, non elementum dolor auctor ut. Morbi risus quam, placerat quis enim quis, dapibus rhoncus augue. Mauris eu condimentum tellus. Phasellus sit amet justo ut felis ullamcorper consectetur nec blandit metus.',btnTitle:'Gyerünk'},
   ]
   developers:Array<object> = [
-    {name:'Ballér Krisztián', photoUrl:'',position:['Fejlesztő']},
-    {name:'Balogh Richárd', photoUrl:'',position:['Fejlesztő','Grafikus']},
-    {name:'Bozzay Viktor', photoUrl:'',position:['Fejlesztő']},
-    {name:'Domján Dominika', photoUrl:'',position:['Fejlesztő']},
-    {name:'Ilosvay Áron', photoUrl:'',position:['Fejlesztő']},
-    {name:'Kertész Károly', photoUrl:'',position:['Fejlesztő']},
-    {name:'Ádám László', photoUrl:'',position:['']},
-    {name:'Kunszt Norbert', photoUrl:'',position:['Fejlesztő']},
-    {name:'Rost Adrienn', photoUrl:'',position:['Fejlesztő','Analitikus','pénzügyes']},
-    {name:'Tóthné Pásztor Mónika', photoUrl:'',position:['Fejlesztő']},
+    {name:'Ballér Krisztián',position:['Fejlesztő'],id:5},
+    {name:'Balogh Richárd', position:['Fejlesztő','Grafikus'],id:5},
+    {name:'Bozzay Viktor', position:['Fejlesztő'],id:5},
+    {name:'Domján Dominika', position:['Fejlesztő'],id:5},
+    {name:'Ilosvay Áron', position:['Fejlesztő'],id:5},
+    {name:'Kertész Károly', position:['Fejlesztő'],id:5},
+    {name:'Ádám László', position:[''],id:5},
+    {name:'Kunszt Norbert', position:['Fejlesztő'],id:15},
+    {name:'Rost Adrienn', position:['Fejlesztő','Analitikus','Pénzügyes'],id:5},
+    {name:'Tóthné Pásztor Mónika', position:['Fejlesztő'],id:5},
   ]
   constructor() { }
 
